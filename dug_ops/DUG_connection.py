cd4bba809fd52a422c7640b25ce288e15f00858e
abhimanyuyadav1985 <abhimanyu.yadav@polarcus.com>
2017-09-18 11:19:27 +0400

    Added logging

--- dug_ops/DUG_connection.py
+++ dug_ops/DUG_connection.py
@@ -3,6 +3,14 @@ import pickle,paramiko,os
 from configuration import conn_config_file,Tape_server_dict
 from dug_ops.DUG_ops import transfer_SEGY_check_script
 
+import logging
+logger = logging.getLogger(__name__)
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+formatter = logging.Formatter('%(levelname)-8s %(message)s')
+console.setFormatter(formatter)
+logger.addHandler(console)
+
 class DUG_connection_object(object):
 
     def __init__(self,use_env):
@@ -25,7 +33,7 @@ class DUG_connection_object(object):
         obj_config = pickle.load(file_handler)
         file_handler.close()
         self.DUG_project_name = obj_config.db_name
-        print "The DUG project name is set to : " + str(self.DUG_proj_name)
+        logger.info("The DUG project name is set to : " + str(self.DUG_proj_name))
 
     def DUG_project_path(self):
         file_path = os.path.join(os.getcwd(),
@@ -34,7 +42,7 @@ class DUG_connection_object(object):
         obj_config = pickle.load(file_handler)
         file_handler.close()
         self.DUG_proj_path = obj_config.DUG_segy_path
-        print "The DUG project path is set to: " + self.DUG_proj_path
+        logger.info("The DUG project path is set to: " + self.DUG_proj_path)
 
     def set_transport(self):
         file_path = os.path.join(os.getcwd(),
@@ -48,7 +56,7 @@ class DUG_connection_object(object):
         DUG_pword = obj_config.DUG_pword
         self.transport = paramiko.Transport((host, port))
         self.transport.connect(username=DUG_user, password=DUG_pword)
-        print "Transport for DUG connection now setup.."
+        logger.info("Transport for DUG connection now setup..")
 
 
     def DUG_ws_client(self):
@@ -65,7 +73,7 @@ class DUG_connection_object(object):
         self.ws_client = paramiko.SSHClient()
         self.ws_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         self.ws_client.connect(host, username=DUG_user, password=DUG_pword)
-        print "The DUG WS client is now active .."
+        logger.info("The DUG WS client is now active ..")
 
 
     def DUG_tape_server_client(self):
@@ -80,7 +88,7 @@ class DUG_connection_object(object):
         self.ts_client = paramiko.SSHClient()
         self.ts_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         self.ts_client.connect(host, username=DUG_user, password=DUG_pword)
-        print "The DUG tape server client is now active .."
+        logger.info("The DUG tape server client is now active ..")
 
 
     def DUG_sftp_client(self):
@@ -96,7 +104,7 @@ class DUG_connection_object(object):
         dug_path = obj_config.DUG_segy_path
         project_name = obj_config.db_name
         self.sftp_client = paramiko.SFTPClient.from_transport(self.transport)
-        print "The DUG sftp client is now active .. "
+        logger.info("The DUG sftp client is now active .. ")
 
 
     def transfer_SEGY_checking_package(self):

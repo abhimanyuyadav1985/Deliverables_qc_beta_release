93c1c1119f93c410876bd507db7600fa62336252
abhimanyuyadav1985 <abhimanyu.yadav@polarcus.com>
2017-09-19 15:45:52 +0400

    Added times logging with decorators

--- database_engine/DB_ops.py
+++ database_engine/DB_ops.py
@@ -5,6 +5,7 @@ from configuration import *
 from unipath import Path
 import sys
 from sqlalchemy import or_,and_
+import time
 
 import logging
 from app_log import  stream_formatter
@@ -15,9 +16,24 @@ formatter = logging.Formatter(stream_formatter)
 console.setFormatter(formatter)
 logger.addHandler(console)
 
+from functools import wraps
+
+def logger_util(func):
+    @wraps(func)
+    def with_logging(*args, **kwargs):
+        ts = time.time()
+        result = func(*args,**kwargs)
+        te = time.time()
+        time_string = "{:8.5f} sec".format(te-ts)
+        logger.info("Finished executing: " + func.__name__ + "  <Execution time => " + time_string)
+        return result
+    return with_logging
+
+
 #------------------------------------------------------------------------------------------------------------
 #Function to check the connection to the desired database_engine in from configuration setup
 #------------------------------------------------------------------------------------------------------------
+@logger_util
 def test_DB_connection(curr):
     logger.info("Testing the DB connection now ......")
     try:
@@ -34,6 +50,7 @@ def test_DB_connection(curr):
         logger.error(error)
         return False
 
+@logger_util
 def test_db_connection_for_config(obj):
     db_name = str(obj.db_name.text())
     db_user = str(obj.db_user.text())
@@ -55,6 +72,7 @@ def test_db_connection_for_config(obj):
         logger.error("Exiting application now")
         sys.exit()
 
+@logger_util
 def fetch_config_info():
     file_path = os.path.join(os.getcwd(), conn_config_file)
     logger.info("config_file: " + file_path)
@@ -72,6 +90,7 @@ def fetch_config_info():
 #-----------------------------------------------------------------------------------------------------------
 # Get the project information dictionary
 #-----------------------------------------------------------------------------------------------------------
+@logger_util
 def fetch_project_info(obj):
     search_result = obj.sess.query(obj.Project_info).order_by(obj.Project_info.ip).all()
     return search_result
@@ -80,6 +99,7 @@ def fetch_project_info(obj):
 #-------------------------------------------------------------------------------------------------------------
 # Deliverables functions
 #--------------------------------------------------------------------------------------------------------------
+@logger_util
 def fetch_deliverables_list(obj):
     search_result  = obj.sess.query(obj.Deliverables).order_by(obj.Deliverables.id).all()
     search_list = []
@@ -87,6 +107,7 @@ def fetch_deliverables_list(obj):
         search_list.append(deliliverable_item.__dict__)
     return search_list
 
+@logger_util
 def fetch_shipments_list(obj):
     search_result = obj.sess.query(obj.Shipments).order_by(obj.Shipments.id).all()
     search_list = []
@@ -94,30 +115,34 @@ def fetch_shipments_list(obj):
         search_list.append(shipment_item.__dict__)
     return search_list
 
+@logger_util
 def add_deliverable(obj, dao_object):
     obj.sess.add(dao_object)
     obj.sess.commit()
 
+@logger_util
 def add_shipment(obj,dao_object):
     obj.sess.add(dao_object)
     obj.sess.commit()
 
+@logger_util
 def fetch_single_deliverable(obj,id):
     search_result = obj.sess.query(obj.Deliverables).filter(obj.Deliverables.id == id).all()
     return search_result[0].__dict__
 
+@logger_util
 def delete_deliverable_obj(obj,id):
-
     deliverable_to_delete = obj.sess.query(obj.Deliverables).filter(obj.Deliverables.id == id)
     deliverable_to_delete.delete()
     obj.sess.commit()
 
+@logger_util
 def delete_shipment_obj(obj,id):
     shipment_to_delete = obj.sess.query(obj.Shipments).filter(obj.Shipments.id == id)
     shipment_to_delete.delete()
     obj.sess.commit()
 
-
+@logger_util
 def add_deliverable_data_dir(obj,dao_object):
     if obj.sess.query(obj.Deliverables_data_dir).filter(obj.Deliverables_data_dir.path == dao_object.path).count() >0:
         logger.warning(dao_object.path + "  exists in database ..")
@@ -128,6 +153,7 @@ def add_deliverable_data_dir(obj,dao_object):
         obj.sess.commit()
         return False
 
+@logger_util
 def add_deliverable_qc_dir(obj,dao_object):
     if obj.sess.query(obj.Deliverables_qc_dir).filter(obj.Deliverables_qc_dir.path == dao_object.path).count() > 0:
         logger.warning(dao_object.path + "  exists in database ..")
@@ -138,6 +164,7 @@ def add_deliverable_qc_dir(obj,dao_object):
         obj.sess.commit()
         return False
 
+@logger_util
 def verify_if_deliverable_id_is_correct(obj,id):
     deliverable_list = obj.sess.query(obj.Deliverables).order_by(obj.Deliverables.id).all()
     id_list = []
@@ -148,11 +175,12 @@ def verify_if_deliverable_id_is_correct(obj,id):
     else:
         return False
 
+@logger_util
 def get_deliverable_object(obj,id):
     deliverable =  obj.sess.query(obj.Deliverables).filter(obj.Deliverables.id == id).first()
     return deliverable
 
-
+@logger_util
 def check_and_add_raw_seq_info(obj):
     cmd = """
         SELECT l.sequence_number AS seq,
@@ -203,14 +231,17 @@ def check_and_add_raw_seq_info(obj):
                logger.info("done...")
     obj.sess.commit()
 
+@logger_util
 def get_deliverables_dao_objects(obj):
     search_result = obj.sess.query(obj.Deliverables).order_by(obj.Deliverables.id).all()
     return search_result
 
+@logger_util
 def get_segd_qc_path(obj,id,set_no):
     search_obj = obj.sess.query(obj.Deliverables_qc_dir).filter(obj.Deliverables_qc_dir.deliverable_id == id).filter(obj.Deliverables_qc_dir.set_number == set_no).first()
     return search_obj
 
+@logger_util
 def get_list_of_applicable_SEGD_tapes(obj, seq_name):
     seq_from_raw_seq_info = obj.sess.query(obj.Raw_seq_info).filter(obj.Raw_seq_info.real_line_name == seq_name).first()
     if seq_from_raw_seq_info == None:
@@ -223,43 +254,52 @@ def get_list_of_applicable_SEGD_tapes(obj, seq_name):
             tape_list.append(obj.name)
         return tape_list
 
+@logger_util
 def get_min_max_ffid_tuple_for_tape(obj,tape_name):
     entry = obj.sess.query(obj.SEGD_tapes).filter(obj.SEGD_tapes.name == tape_name).first()
     min_ffid = entry.first_ffid
     max_ffid = entry.last_ffid
     return (min_ffid,max_ffid)
 
+@logger_util
 def get_list_of_segd_deliverables(obj):
     deliverables_list = obj.sess.query(obj.Deliverables).filter(obj.Deliverables.class_d == 'SEGD').order_by(obj.Deliverables.id).all()
     return deliverables_list
 
+@logger_util
 def get_all_available_segd_tapes_in_orca(obj):
     tape_obj_list = obj.sess.query(obj.SEGD_tapes).order_by(obj.SEGD_tapes.name).all()
     return tape_obj_list
 
+@logger_util
 def check_previous_passed_for_SEGD_qc(obj,id,set_no):
     result = obj.sess.query(obj.SEGD_qc).filter(obj.SEGD_qc.qc_status == True, obj.SEGD_qc.deliverable_id == id,obj.SEGD_qc.set_no == set_no).order_by(obj.SEGD_qc.tape_no).all()
     return result
 
+@logger_util
 def check_previous_run_for_SEGD_qc(obj,id,set_no):
     result = obj.sess.query(obj.SEGD_qc).filter(obj.SEGD_qc.run_status == True, obj.SEGD_qc.deliverable_id == id,
                                                 obj.SEGD_qc.set_no == set_no).order_by(obj.SEGD_qc.tape_no).all()
     return result
 
+@logger_util
 def fetch_seq_id_from_name(obj,seq_name):
     seq_from_raw_seq_info = obj.sess.query(obj.Raw_seq_info).filter(obj.Raw_seq_info.real_line_name == seq_name).first()
     seq_id = seq_from_raw_seq_info.seq
     return seq_id
 
+@logger_util
 def add_SEGD_QC_obj(obj,dao_obj):
     logger.info("Now adding to SEGD QC DB Table: " + str(dao_obj.deliverable_id)+ " Set no: " + str(dao_obj.set_no) + " Tape no: " + dao_obj.tape_no)
     obj.sess.add(dao_obj)
     obj.sess.commit()
 
+@logger_util
 def get_all_production_sequences(obj):
     prod_seq_list = obj.sess.query(obj.Raw_seq_info).order_by(obj.Raw_seq_info.seq).all()
     return prod_seq_list
 
+@logger_util
 def fetch_seq_name_from_id(obj,seq_id):
     seq_from_raw_seq_info = obj.sess.query(obj.Raw_seq_info).filter(obj.Raw_seq_info.seq == seq_id).first()
     if seq_from_raw_seq_info is not None:
@@ -268,27 +308,32 @@ def fetch_seq_name_from_id(obj,seq_id):
         seq_name = "NTBP"
     return seq_name
 
+@logger_util
 def get_all_SEGD_qc_entries(obj):
     segd_qc_list = obj.sess.query(obj.SEGD_qc).order_by(obj.SEGD_qc.tape_no).all()
     return segd_qc_list
 
+@logger_util
 def get_project_name(obj):
     project_info = obj.sess.query(obj.Project_info).order_by(obj.Project_info.ip).first()
     project_name = project_info.project
     return project_name
-
+@logger_util
 def get_list_of_SEGY_deliverables(obj):
     deliverables_list = obj.sess.query(obj.Deliverables).filter(obj.Deliverables.class_d == 'SEGY').order_by(obj.Deliverables.id).all()
     return deliverables_list
 
+@logger_util
 def get_list_of_seq_SEGY_deliverables(obj):
     deliverables_list = obj.sess.query(obj.Deliverables).filter(obj.Deliverables.class_d == 'SEGY').filter(obj.Deliverables.type == 'SEQG').order_by(obj.Deliverables.id).all()
     return deliverables_list
 
+@logger_util
 def get_SEGD_QC_object_list_for_deliverable_set(obj,deliverable_id,set_no):
     obj_list = obj.sess.query(obj.SEGD_qc).filter(obj.SEGD_qc.deliverable_id == deliverable_id ).filter(obj.SEGD_qc.set_no == set_no).order_by(obj.SEGD_qc.seq_id).all()
     return obj_list
 
+@logger_util
 def check_and_add_media_list_obj(obj,media_obj):
     status = obj.sess.query(obj.Media_list).filter(obj.Media_list.deliverable_id == media_obj.deliverable_id).filter(obj.Media_list.set_no == media_obj.set_no).filter(obj.Media_list.media_label == media_obj.media_label).first()
     if status == None:
@@ -297,26 +342,32 @@ def check_and_add_media_list_obj(obj,media_obj):
     else:
         pass
 
+@logger_util
 def fetch_shipment_objects_list(obj):
     obj_list = obj.sess.query(obj.Shipments).order_by(obj.Shipments.id).all()
     return obj_list
 
+@logger_util
 def fetch_deliverable_objects_list(obj):
     obj_list = obj.sess.query(obj.Deliverables).order_by(obj.Deliverables.id).all()
     return obj_list
 
+@logger_util
 def fetch_media_list_did_set(obj,deliverable_id,set_no):
     obj_list = obj.sess.query(obj.Media_list).filter(obj.Media_list.deliverable_id ==  deliverable_id).filter(obj.Media_list.set_no == set_no).all()
     return obj_list
 
+@logger_util
 def add_usb_list_obj(obj, new_usb_label):
     obj.sess.add(new_usb_label)
     obj.sess.commit()
 
+@logger_util
 def check_usb_list_obj(obj, new_usb_name):
     result = obj.sess.query(obj.USB_list).filter(obj.USB_list.label == new_usb_name).first()
     return result
 
+@logger_util
 def fetch_usb_list_dict(obj):
     search_result = obj.sess.query(obj.USB_list).order_by(obj.USB_list.usb_id).all()
     search_list = []
@@ -324,47 +375,56 @@ def fetch_usb_list_dict(obj):
         search_list.append(item.__dict__)
     return search_list
 
+@logger_util
 def delete_usb_list_obj(obj,id):
     obj_to_delete = obj.sess.query(obj.USB_list).filter(obj.USB_list.usb_id == id)
     #print deliverable_to_delete
     obj_to_delete.delete()
     obj.sess.commit()
 
+@logger_util
 def get_all_available_usb(obj):
     result = obj.sess.query(obj.USB_list).order_by(obj.USB_list.usb_id).all()
     return result
 
+@logger_util
 def get_all_SEGY_deliverable_for_tape_write(obj):
     result = obj.sess.query(obj.Deliverables).filter(obj.Deliverables.class_d == 'SEGY').filter(or_(obj.Deliverables.media == '3592 JA', obj.Deliverables.media == '3592 JC',
                                                                                                     obj.Deliverables.media == '3592 JA multiple', obj.Deliverables.media =='3592 JC multiple')).all()
     return result
 
+@logger_util
 def return_SEGD_QC_log_path(obj, tape_no, set_no):
     result = obj.sess.query(obj.SEGD_qc).filter(and_(obj.SEGD_qc.tape_no == tape_no, obj.SEGD_qc.set_no == set_no)).first()
     return result.log_path
 
+@logger_util
 def return_shipment_content_from_number(obj,shipment_no):
     result = obj.sess.query(obj.Media_list).filter(obj.Media_list.shipment_no == shipment_no).order_by(obj.Media_list.box_no).order_by(obj.Media_list.reel_no).all()
     return result
 
 
+@logger_util
 def get_all_SEGD_QC_for_deliverable(obj,deliverable_id):
     obj_list = obj.sess.query(obj.SEGD_qc).filter(obj.SEGD_qc.deliverable_id == deliverable_id).order_by(obj.SEGD_qc.set_no).order_by(obj.SEGD_qc.tape_no).all()
     return obj_list
 
-
+@logger_util
 def get_all_SEGY_write_objects(obj):
     obj_list = obj.sess.query(obj.SEGY_write).all()
     return obj_list
 
+@logger_util
 def get_all_SEGY_qc_objects(obj, deliverable_id):
     obj_list = obj.sess.query(obj.SEGY_QC_on_disk).filter(obj.SEGY_QC_on_disk.deliverable_id == deliverable_id).all()
     return obj_list
 
+@logger_util
 def get_all_segd_objects_for_set_checked_before(obj,deliverable_id, set_no):
     obj_list = obj.sess.query(obj.SEGD_qc).filter(obj.SEGD_qc.deliverable_id == deliverable_id).filter(obj.SEGD_qc.set_no == set_no).filter(obj.SEGD_qc.qc_status == True).all()
     return obj_list
 
+@logger_util
 def get_all_raw_seq_info_objects(obj):
     obj_list = obj.sess.query(obj.Raw_seq_info).all()
     return obj_list

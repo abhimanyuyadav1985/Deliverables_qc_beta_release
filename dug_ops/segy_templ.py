e2bc18d894fb807df0ad406e82ad88307472848e
abhimanyuyadav1985 <abhimanyu.yadav@polarcus.com>
2017-09-24 14:42:54 +0400

    now only changes the first occurence of lino in sgyt

--- dug_ops/segy_templ.py
+++ dug_ops/segy_templ.py
@@ -59,10 +59,14 @@ def create_sgyt(deliverable,sequence,IL_range,XL_range,reel):
 
         a[0] = 'ebcdic_header ' + ebcdic.encode('base64')
         a[0] = a[0].replace('\n', '')
-
+        line_done = False
         for s in a:
             if 'lino' in s:
-                s = '    lino {0:s} '.format(sequence.preplot_name)
+                if line_done:
+                    pass
+                else:
+                    s = '    lino {0:s} '.format(sequence.preplot_name)
+                    lino_done = True
             if 'reno' in s:
                 s = '    reno ' + str(int(reel))
 

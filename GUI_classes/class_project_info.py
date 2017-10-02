591373948cd3cb198d73fdb0c401bda6eb7d623a
abhimanyuyadav1985 <abhimanyu.yadav@polarcus.com>
2017-10-02 23:32:10 +0400

    Now handles the case when the user will use applicaiton before project info is present in orca.project_info

--- GUI_classes/class_project_info.py
+++ GUI_classes/class_project_info.py
@@ -12,15 +12,19 @@ class project_info(QtGui.QScrollArea):
         self.db_connection_obj = self.parent.db_connection_obj  # use this line to pass the connection object between the parent and the child
         self.project_info = fetch_project_info(self.db_connection_obj)
         grid = QtGui.QGridLayout()
-        info_dict = self.project_info[0].__dict__
-        j = 0
-        grid.addWidget(create_central_labels('General Project information'),0,0,1,2)
-        for key in info_dict:
-            if key != '_sa_instance_state':
-                j= j+1
-                #print key , info_dict[key]
-                grid.addWidget(create_center_data(key),j,0)
-                grid.addWidget(create_left_blank(str(info_dict[key]),'blue'),j,1)
+        grid.addWidget(create_central_labels('General Project information'), 0, 0, 1, 2)
+        if self.project_info == None or len(self.project_info) == 0:
+            grid.addWidget(create_center_data("Project information from ORCA is not available !!!"),1,0,1,2)
+        else:
+            info_dict = self.project_info[0].__dict__
+            j = 0
+
+            for key in info_dict:
+                if key != '_sa_instance_state':
+                    j= j+1
+                    #print key , info_dict[key]
+                    grid.addWidget(create_center_data(key),j,0)
+                    grid.addWidget(create_left_blank(str(info_dict[key]),'blue'),j,1)
 
         self.setLayout(grid)
 
